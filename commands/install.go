26ff934aaa04521296ab62517932e0b143505b8a
Jason Waldrip <jason@waldrip.net>
2014-08-30 22:18:25 -0600

    Extracts and Prints Tar Contents

--- commands/install.go
+++ commands/install.go
@@ -3,23 +3,69 @@ package commands
 import "fmt"
 import "net/http"
 import "io/ioutil"
+import "io"
 import "os"
+import "archive/tar"
+import "compress/gzip"
+import "log"
+import "bytes"
 
 func Install(args []string){
   version := args[0]
-  fmt.Println("Install Ruby", getVersionString(version))
+  // fmt.Println("Install Ruby", getVersionString(version))
+  downloadPackage(version)
 }
 
 func downloadPackage(version string){
   version = getVersionString(version)
+  url := fmt.Sprintf("http://ruby.itri.ag/versions/%v/binary", version)
+  fmt.Println("Downloading and installing ruby", version)
+  tarUnarchive(gzipExtract(getBodyIO(url)))
+}
+
+func tarUnarchive(buffer *bytes.Buffer, dest string){
+    // Read the tar
+    tr := tar.NewReader(buffer)
+    for {
+      hdr, err := tr.Next()
+      if err == io.EOF {
+        break
+      }
+      if err != nil {
+        log.Fatalln(err)
+      }
+      // fmt.Println(hdr.Typeflag, hdr.Name)
+      switch {
+        case hdr.FileInfo().IsDir():
+          fmt.Println("Dir:", hdr.Name)
+        default:
+          fmt.Println("File:", hdr.Name)
+      }
+    }
+}
+
+func gzipExtract(ioVar io.ReadCloser) *bytes.Buffer {
+  defer ioVar.Close()
+  gr, err := gzip.NewReader(ioVar)
+  defer gr.Close()
+  if err != nil {
+    fmt.Println(fmt.Sprintf("%s", err))
+    os.Exit(1)
+  }
+  outBytes, err := ioutil.ReadAll(gr)
+  if err != nil {
+    fmt.Println(fmt.Sprintf("%s", err))
+    os.Exit(1)
+  }
+  return bytes.NewBuffer(outBytes)
 }
 
 func getVersionString(partial string) string {
   url := fmt.Sprintf("http://ruby.itri.ag/versions/%v.txt", partial)
-  return string(getBody(url))
+  return string(getBodyString(url))
 }
 
-func getBody(url string) []byte {
+func getBodyIO(url string) io.ReadCloser {
   resp, err := http.Get(url)
   if err != nil {
     fmt.Println(fmt.Sprintf("%s", err))
@@ -28,11 +74,16 @@ func getBody(url string) []byte {
     fmt.Println(fmt.Sprintf("%s", resp.Status))
     os.Exit(1)
   }
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
+  return resp.Body
+}
+
+func getBodyString(url string) string {
+  ioBody := getBodyIO(url)
+  defer ioBody.Close()
+  body, err := ioutil.ReadAll(ioBody)
   if err != nil {
     fmt.Println(fmt.Sprintf("%s", err))
     os.Exit(1)
   }
-  return body
+  return string(body)
 }
